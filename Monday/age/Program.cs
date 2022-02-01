94f5ef60da425a88096c300e2c3313f843d8d48f
Shrivallibhat <shrivallialevoor@gmail.com>
2022-02-01 09:43:50 +0530

    problems

---
+++ Monday/age/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            Console.WriteLine("enter age :");
+            age = Convert.ToInt32(Console.ReadLine());
+            if(age>=18)
+            {
+                Console.WriteLine("person is allowed foe voting:" + age);
+
+            }
+            else
+            {
+                Console.WriteLine("user is nor allowed to vote:" + age);
+            }
+        }
+    }
+}
---
+++ Monday/greatest of three/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace greatest_of_three
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+            // set the value of the three numbers
+            num1 = 12;
+            num2 = 60;
+            num3 = 18;
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                {
+                    Console.Write("Number one is the largest!\n");
+                }
+                else
+                {
+                    Console.Write("Number three is the largest!\n");
+                }
+            }
+            else if (num2 > num3)
+                Console.Write("Number two is the largest!\n");
+            else
+                Console.Write("Number three is the largest!\n");
+        }
+    }
+}
+
