e376df05186aff88df7870a4b1c9c3ba4a0ab073
Shrivallibhat <shrivallialevoor@gmail.com>
2022-02-03 16:11:51 +0530

    thursdayl

---
+++ Thursday(3-4-22)/pblm1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+public class StudentAndTeacherTest
+{
+    public static void Main()
+    {
+        Person myPerson = new Person();
+        myPerson.Greet();
+
+        Student myStudent = new Student();
+        myStudent.SetAge(20);
+        myStudent.Greet();
+        myStudent.ShowAge();
+        myStudent.Study();
+
+        Teacher myTeacher = new Teacher();
+        myTeacher.SetAge(35);
+        myTeacher.Greet();
+        myTeacher.Explain();
+    }
+}
+
+public class Person
+{
+    protected int age;
+
+    public void Greet()
+    {
+        Console.WriteLine("Hello");
+    }
+
+    public void SetAge(int n)
+    {
+        age = n;
+    }
+}
+
+public class Student : Person
+{
+    public void Study()
+    {
+        Console.WriteLine("I'm stdent and I'm studying");
+    }
+
+    public void ShowAge()
+    {
+        Console.WriteLine("My age is {0} years old", age);
+    }
+}
+
+public class Teacher : Person
+{
+    public void Explain()
+    {
+        Console.WriteLine("Teacher");
+        Console.WriteLine("I'm explaining");
+    }
+}
---
+++ Thursday(3-4-22)/pblm6/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Shapes
+{
+    class Square
+    {
+        private int x;
+        private int y;
+        private int length;
+        
+        public Square(int x, int y, int length)
+        {
+            this.x = x;
+            this.y = y;
+            this.length = length;
+        }
+
+        public void Move(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        
+        public void Scale(int scaleFactor)
+        {
+            this.length *= scaleFactor;
+        }
+        public override string ToString()
+        {
+            return string.Format("Corner ({0},{1}), side {2}", this.x, this.y, this.length);
+        }
+        
+        public double GetPerimeter()
+        {
+            return 4 * this.length;
+        }
+        public double GetArea()
+        {
+            return this.length * this.length;
+        }
+    }
+
+    class squareshape
+    {
+        static void Main(string[] args)
+        {
+            Square square = new Square(5, 5, 10);
+            Console.WriteLine(square.ToString());
+            Console.WriteLine("\nMove(15, 20)");
+            square.Move(15, 20);
+            Console.WriteLine(square.ToString());
+            Console.WriteLine("\nScale(3)");
+            square.Scale(3);
+            Console.WriteLine(square.ToString());
+            Console.WriteLine("\nThe perimeter = {0}", square.GetPerimeter());
+            Console.WriteLine("\nThe area = {0}", square.GetArea());
+
+
+            Console.ReadLine();
+        }
+    }
+}
