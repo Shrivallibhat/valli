d74608c0afae52d8437347c498d0825bb252b005
Shrivallibhat <shrivallialevoor@gmail.com>
2022-02-08 16:48:01 +0530

    8-2-22

---
+++ tuesday-(8-2-22)/pblm5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+public class ConvertCelciusToKelvinAndFahrenheit
+{
+    public static void Main(string[] args)
+    {
+        int celsius = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("kelvin= {0}", celsius + 273);
+        Console.WriteLine("fahrenheit= {0}", celsius * 18 / 10 + 32);
+    }
+}
+
---
+++ tuesday-(8-2-22)/pblm6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+public class CheckPalindromeRecursively
+{
+    public static void Main(string[] args)
+    {
+        string text = Console.ReadLine();
+        Console.WriteLine(IsPalindrome(text));
+    }
+
+    public static bool IsPalindrome(string text)
+    {
+        if (text.Length <= 1)
+        {
+            return true;
+        }
+        else
+        {
+            if (text[0] != text[text.Length - 1])
+            {
+                return false;
+            }
+            else
+            {
+                return IsPalindrome(text.Substring(1, text.Length - 2));
+            }
+        }
+    }
+}
