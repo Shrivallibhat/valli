93601be40758c9ebcf6df7cb20e595003e5f5550
Shrivallibhat <shrivallialevoor@gmail.com>
2022-02-09 17:19:50 +0530

    9-7-22

---
+++ wednesday(9-2-22)/pblm3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collections.Generics
+{
+    internal class DictionaryExample
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Example of Dictionary");
+            Dictionary<int, string> PL = new Dictionary<int, string>();
+            Console.WriteLine("Please enter name of languages");
+            for(int i = 1; i < 3; i++)
+            {
+               PL.Add(i,Console.ReadLine());
+            }
+            foreach (var item in PL)
+            {
+                Console.WriteLine(item.Key+"====>"+item.Value);
+            }
+            */
+            Console.WriteLine("Example of List");
+            List<string> list = new List<string>();
+            Console.WriteLine("Please enter name of languages");
+            for (int i = 1; i < 3; i++)
+            {
+                list.Add(Console.ReadLine());
+            }
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
---
+++ wednesday(9-2-22)/pblm4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collections.Generics
+{
+    internal class DictionaryExample
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Example of Dictionary");
+            Dictionary<int, string> PL = new Dictionary<int, string>();
+            Console.WriteLine("Please enter name of languages");
+            for(int i = 1; i < 3; i++)
+            {
+               PL.Add(i,Console.ReadLine());
+            }
+            foreach (var item in PL)
+            {
+                Console.WriteLine(item.Key+"====>"+item.Value);
+            }
+            */
+            Console.WriteLine("student class");
+            List<string> list = new List<string>();
+            Console.WriteLine("please enter the student id:");
+            Console.WriteLine("Please enter name of student");
+            for (int i = 1; i < 3; i++)
+            {
+                list.Add(Console.ReadLine());
+            }
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
