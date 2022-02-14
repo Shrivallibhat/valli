322359dc62b9bba5821808b684cd077a8883eaba
Shrivallibhat <shrivallialevoor@gmail.com>
2022-02-14 11:03:45 +0530

    14-2-22

---
+++ Monday(14-2-22)/pblm 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+public class Stringexercise
+{
+    public static void Main()
+    {
+        string str1;
+        char[] arr1;
+        int length, i;
+        length = 0;
+        char ch;
+        Console.Write("Enter the string : ");
+        str1 = Console.ReadLine();
+        length = str1.Length;
+        arr1 = str1.ToCharArray(0, length); // Converts string into char array.
+
+        Console.Write("\nAfter conversion, the string is: ");
+        for (i = 0; i < length; i++)
+        {
+            ch = arr1[i];
+            if (Char.IsLower(ch)) // check whether the character is lowercase
+                Console.Write(Char.ToUpper(ch)); // Converts lowercase character to uppercase.
+            else
+                Console.Write(Char.ToLower(ch)); // Converts uppercase character to lowercase.
+        }
+        Console.ReadLine();
+    }
+}
